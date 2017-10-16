cc676ab13d2f362809596450fd8196bb21f4c6ac
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-10-16 14:15:30 -0400

    array package is in

---
+++ Islam/src/arrays/ArraysMain.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ArraysMain {
+
+	public ArraysMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
