2e47259e207361185796a68237963b9678df44f6
nasir <nasir@nas1>
2017-12-03 23:32:46 -0500

    nhg

---
+++ Islam/src/search/TowersOfHanoi.java
@@ -0,0 +1,97 @@
+package search;
+
+public class TowersOfHanoi {
+	
+	enum Tower
+	{
+		START,
+		HELPER,
+		END
+		
+	}
+
+	public TowersOfHanoi(int n) 
+	{
+		String start = "";
+		for(int i = n; i > 1; i--) 
+		{
+			start += i;
+		}
+		solve(n, start, "", "");
+	}
+	
+	public void solve(int n, String start, String helper, String end)
+	{
+		
+		if(n == 1) 
+		{
+			if(start == "" && helper == "")
+			{
+				//Everything is empty then everything has been moved
+				return;
+			}
+			else 
+			{
+				if(start != "" && helper == "") 
+				{
+					//if start is empty, it is time to start from the top
+					solve(getTopNum(start), start, helper, end);
+				}
+				else 
+				{
+					// this condition should never be reached
+				}
+			}
+		}
+	
+		if(start.length() != 0 && getTopNum(start) == n) 
+		{
+			//if things need to be moved from start moved them
+			boolean moved = false;
+			if(n % 2 == 0) 
+			{
+				//if even and can be moved, move to end
+			}
+			else
+			{
+				//if odd move and can be moved, move to helper
+			}
+			
+			if(!moved)
+			{
+				
+				//if it cannot move, move the smaller problem
+				solve(n-1, start, helper, end);
+			}
+		}
+		else if(getTopNum(helper) ==n || getTopNum(end) == n) 
+		{
+			//if the number is not at start
+			//move it to the appropriate pile, depending where it fits
+			if(getTopNum(helper) == n + 1) 
+			{
+				//move to helper
+			}
+			else if(getTopNum(end) == n + 1) 
+			{
+				//move to end
+			}
+		}
+		else 
+		{
+			//if none of these are true then the layer on top needs to be moved before this layer can be moved
+			solve(n-1, start, helper, end);
+		}
+	}
+	
+	public String removeEndChar(String s) 
+	{
+		return s.substring(0, s.length()-2);
+	}
+	
+	public int getTopNum(String s) 
+	{
+		return Integer.parseInt(s.substring(s.length()-1,s.length()));
+	}
+}
+
